5bacca1bc5a52d7ac6a1ca3bac2a77beb6a65f74
lustered <smellslikelavenders@gmail.com>
2020-11-23 18:54:23 -0500

    xrandr and xset

---
+++ .monitors.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+xrandr --output DP-2 --mode 2560x1440 --rate 143.99
+xrandr --output HDMI-0 --mode 1920x1080 --rotate left --rate 60.00
+xset r rate 155 90
+# sleep 3m; sudo insmod 8812au.ko
