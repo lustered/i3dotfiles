cf8a35900488f67b28d7d2d8a3cdcc256ebb904d
lustered <smellslikelavenders@gmail.com>
2021-04-02 17:34:52 -0400

    .

---
+++ tv.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+xrandr --output DP-2 --mode 2560x1440 --rate 144.00
+xrandr --output HDMI-0 --mode 1920x1080 --rotate normal --rate 59.99
+xset r rate 200 100
--- update.sh
+++ update.sh
@@ -6,3 +6,8 @@ cp ~/.config/kitty/kitty.conf kitty.conf
 cp -r ~/.config/polybar/colorblocks/ polybar
 cp ~/.vim/plugged/vim-gruvbox8/colors/gruvbox8_hard.vim gruvbox8_hard.vim
 cp ~/.config/rofi/bliss.rasi bliss.rasi
+cp ~/.Xresources Xresources
+cp ~/dotfiles/monitors.sh monitors.sh
+cp ~/dotfiles/tv.sh tv.sh
+cp ~/.vim/coc-vim.vim coc-vim.vim
+cp ~/.vim/coc-settings.json coc-settings.json
